c3f1bf3d7850f64c114e65648e36aeed688e1800
Alessandro Calzavara <alessandro.calzavara@gmail.com>
2023-01-25 12:24:01 +0100

    Second

--- amplify/backend/custom/storage/cdk-stack.ts
+++ amplify/backend/custom/storage/cdk-stack.ts
@@ -2,9 +2,6 @@ import * as cdk from '@aws-cdk/core';
 import * as AmplifyHelpers from '@aws-amplify/cli-extensibility-helper';
 import { AmplifyDependentResourcesAttributes } from '../../types/amplify-dependent-resources-ref';
 import * as iam from '@aws-cdk/aws-iam';
-//import * as sns from '@aws-cdk/aws-sns';
-//import * as subs from '@aws-cdk/aws-sns-subscriptions';
-//import * as sqs from '@aws-cdk/aws-sqs';
 
 export class cdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps, amplifyResourceProps?: AmplifyHelpers.AmplifyResourceProps) {
@@ -16,38 +13,6 @@ export class cdkStack extends cdk.Stack {
     });
     /* AWS CDK code goes here - learn more: https://docs.aws.amazon.com/cdk/latest/guide/home.html */
     
-    // Example 1: Set up an SQS queue with an SNS topic 
-
-    /*
-    const amplifyProjectInfo = AmplifyHelpers.getProjectInfo();
-    const sqsQueueResourceNamePrefix = `sqs-queue-${amplifyProjectInfo.projectName}`;
-    const queue = new sqs.Queue(this, 'sqs-queue', {
-      queueName: `${sqsQueueResourceNamePrefix}-${cdk.Fn.ref('env')}`
-    });
-    // 👇create sns topic
-    
-    const snsTopicResourceNamePrefix = `sns-topic-${amplifyProjectInfo.projectName}`;
-    const topic = new sns.Topic(this, 'sns-topic', {
-      topicName: `${snsTopicResourceNamePrefix}-${cdk.Fn.ref('env')}`
-    });
-    // 👇 subscribe queue to topic
-    topic.addSubscription(new subs.SqsSubscription(queue));
-    new cdk.CfnOutput(this, 'snsTopicArn', {
-      value: topic.topicArn,
-      description: 'The arn of the SNS topic',
-    });
-    */
-
-    // Example 3: Adding policy to the IAM role
-    /*
-    role.addToPolicy(
-      new iam.PolicyStatement({
-        actions: ['*'],
-        resources: [topic.topicArn],
-      }),
-    );
-    */
-
     // Access other Amplify Resources 
     const retVal:AmplifyDependentResourcesAttributes = AmplifyHelpers.addResourceDependency(this, 
       amplifyResourceProps.category, 
@@ -59,12 +24,11 @@ export class cdkStack extends cdk.Stack {
 
     // Adding something in the stack
     const amplifyProjectInfo = AmplifyHelpers.getProjectInfo();
-    const roleResourceNamePrefix = `CustomRole-${amplifyProjectInfo.projectName}`;
+    const roleResourceNamePrefix = `UselessRole-${amplifyProjectInfo.projectName}`;
     
-    const role = new iam.Role(this, 'CustomRole', {
+    const role = new iam.Role(this, 'useless-role', {
       assumedBy: new iam.AccountRootPrincipal(),
       roleName: `${roleResourceNamePrefix}-${cdk.Fn.ref('env')}`
     }); 
-
   }
 }
